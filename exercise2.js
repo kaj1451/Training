b84626bd48aee24e8a1dc3c5ef4b58188581a2f5
kaj1451 <kajendran.suyambulingam@cdw.com>
2025-04-03 12:18:33 +0530

    Add files via upload

---
+++ exercise2.js
@@ -0,0 +1,81 @@
+function demo() {
+  document.getElementById("demo").innerHTML = "Text Changed!";
+}
+function img(){
+  document.body.style.backgroundImage = "url('download.jpg')";
+}
+function display()
+{
+ let name = document.getElementById("names").value;
+ document.getElementById("hello").innerText = `Hello, ${name}`;
+}
+function add(){
+  let ul = document.getElementById("ul");
+  let li = document.createElement("li");
+  li.innerText = "New Item";
+  ul.appendChild(li);
+}
+function remove(){
+  let ul = document.getElementById("ul");
+  if (ul.children.length > 0) {
+        ul.removeChild(ul.lastElementChild);
+    }
+}
+document.getElementById("click").addEventListener("click", function() {alert("alert");});
+
+function timeout() {
+    document.getElementById("message").innerText = "Wait for 3 seconds";
+    
+    setTimeout(function() {
+        document.getElementById("message").innerText = "after 3 seconds";
+    }, 3000);
+}
+function showMessage() {
+    document.getElementById("message").innerText = "Wait for 3 seconds...";
+    
+    setTimeout(function() {
+        document.getElementById("message").innerText = "Naa dhan da leo";
+    }, 3000);
+}
+
+function showAlert() {
+    alert("This is an alert");
+}
+
+function showConfirm() {
+    let result = confirm("Are you sure?");
+    document.getElementById("messagehere").innerText = result ? "You clicked OK!" : "You clicked Cancel!";
+}
+function showPrompt() {
+    let name = prompt("Enter your name:");
+    if (name) {
+        document.getElementById("messagehere").innerText = `Hello, ${name}!`;
+    } else {
+        document.getElementById("messagehere").innerText = "No name entered!";
+    }
+}
+
+document.getElementById("textInput").addEventListener("keyup", function() {
+    document.getElementById("output").innerText = this.value;
+});
+
+function redirect() {
+    window.location.href = "https://www.google.com"; 
+}
+function newtab() {
+    window.open("https://www.google.com"); 
+}
+let counter = 0;
+let intervalId;
+function start() {
+if (!intervalId) {
+        intervalId = setInterval(function() {
+          counter++;
+          document.getElementById("counter").innerText = counter;
+      }, 1000); 
+    }
+}
+function stop() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
